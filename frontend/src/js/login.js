58696d5ab246d0d5cfe27b7b75b4a5e2a5501a01
axvg <91773368+axvg@users.noreply.github.com>
2022-12-29 23:14:46 -0500

    Update login page

--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -1,6 +1,6 @@
 async function login(email, password) {
   try {
-    var response = await fetch('http://localhost:8000/users/login/', {
+    let response = await fetch('http://localhost:8000/users/login/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -20,15 +20,19 @@ async function login(email, password) {
 document.getElementById('login-form').addEventListener('submit', async function(event) {
   event.preventDefault();
 
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
 
-  var response = await login(email, password);
-  if (response) {
-    localStorage.setItem('jwt', response.tokens.access);
-    window.location.href = './index.html';
-    console.log(response.tokens);
+  let response = await login(email, password);
+  let tokens = response?.tokens
+  if (tokens) {
+    localStorage.setItem('jwt', tokens?.access);
+    window.location.replace('./index.html');
   } else {
-    console.log('There was an error logging in. Please check your email and password and try again.');
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Correo inválido o contraseña incorrecta',
+    });
   }
 });
