f395e54b64324bcc550785fe0dec70c21cd96d88
axvg <91773368+axvg@users.noreply.github.com>
2022-12-29 22:43:20 -0500

    Update login page

--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -1,38 +1,34 @@
-import axios from 'axios';
 async function login(email, password) {
-    try {
-      // Send login request to API
-      var response = await axios.post('http://localhost:8000/users/login/', {
+  try {
+    var response = await fetch('http://localhost:8000/users/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
         email: email,
-        password: password
-      });
-      // Return token on successful login
-      return response.data.token;
-    } catch (error) {
-      // Log error and return null on login error
-      console.error(error);
-      return null;
-    }
+        password: password,
+      }),
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+    return null;
   }
+}
 
-// Handle form submission
 document.getElementById('login-form').addEventListener('submit', async function(event) {
-    // Prevent form submission
-    event.preventDefault();
-  
-    // Get form data
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-  
-    // Attempt login
-    var token = await login(email, password);
-    if (token) {
-      // Store token in session storage and redirect to protected page on successful login
-      sessionStorage.setItem('jwt', token);
-      window.location.href = '/protected';
-      console.log('Login correct!')
-    } else {
-      // Display error message on login error
-      alert('There was an error logging in. Please check your email and password and try again.');
-    }
-  });
+  event.preventDefault();
+
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  var response = await login(email, password);
+  if (response) {
+    localStorage.setItem('jwt', response.tokens.access);
+    window.location.href = './index.html';
+    console.log(response.tokens);
+  } else {
+    console.log('There was an error logging in. Please check your email and password and try again.');
+  }
+});
